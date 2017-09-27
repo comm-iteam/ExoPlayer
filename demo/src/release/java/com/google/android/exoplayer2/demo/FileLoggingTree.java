a7a91795eba960c113b77b8ae2005e9bc8209a25
Román Belda <robelor@gmail.com>
2017-09-27 11:35:15 +0200

    Added FileLoggingTree to release

---
+++ demo/src/release/java/com/google/android/exoplayer2/demo/FileLoggingTree.java
@@ -0,0 +1,17 @@
+package com.google.android.exoplayer2.demo;
+
+import android.content.Context;
+import timber.log.Timber;
+
+public class FileLoggingTree extends Timber.DebugTree {
+
+  public FileLoggingTree(Context context) {
+  }
+
+
+
+  @Override
+  protected void log(int priority, String tag, String message, Throwable t) {
+
+  }
+}
